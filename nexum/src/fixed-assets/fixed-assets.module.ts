dc12f2389a3ab9315746d0e5b95d0f66787174e3
a5r8c18 <cobaseliu@gmail.com>
2026-08-14 11:26:54 -0400

    fix(aft): inyectar SupplierRepository en FixedAssetsModule

--- nexum/src/fixed-assets/fixed-assets.module.ts
+++ nexum/src/fixed-assets/fixed-assets.module.ts
@@ -7,6 +7,7 @@ import { DepreciationHistory } from '../entities/depreciation-history.entity';
 import { DepreciationCatalog } from '../entities/depreciation-catalog.entity';
 import { FixedAssetInventory } from '../entities/fixed-asset-inventory.entity';
 import { Employee } from '../entities/employee.entity';
+import { Supplier } from '../entities/supplier.entity';
 import { AuthModule } from '../auth/auth.module';
 import { AccountingModule } from '../accounting/accounting.module';
 import { AuditModule } from '../audit/audit.module';
@@ -14,7 +15,7 @@ import { FinanceModule } from '../finance/finance.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([FixedAsset, DepreciationHistory, DepreciationCatalog, FixedAssetInventory, Employee]),
+    TypeOrmModule.forFeature([FixedAsset, DepreciationHistory, DepreciationCatalog, FixedAssetInventory, Employee, Supplier]),
     forwardRef(() => AuthModule),
     forwardRef(() => AccountingModule),
     forwardRef(() => FinanceModule),
